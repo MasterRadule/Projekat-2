68ae7d962bceb5d9975116dca72859e2cecb6fcd
Danijel Radulović <master.daca09@gmail.com>
2019-10-27 18:24:48 +0100

    Initial commit

---
+++ src/test/java/ktsnvt/tim1/ProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package ktsnvt.tim1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
