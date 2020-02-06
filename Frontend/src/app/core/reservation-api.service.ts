39d479e3480caeb877f696b11210bc782eb6a66f
Aleksandar Nedaković <aleksaleks123@gmail.com>
2020-02-06 16:08:31 +0100

    Finish pay and cancel reservation implementation on frontend

--- Frontend/src/app/core/reservation-api.service.ts
+++ Frontend/src/app/core/reservation-api.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {PaymentDTO} from '../shared/model/payment-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,24 @@ export class ReservationApiService {
 
   constructor(private _http: HttpClient) {
   }
+
   getReservations(reservationType: string, page: number, size: number) {
     return this._http.get(`reservations?page=${page}&size=${size}&type=${reservationType.toUpperCase()}`);
   }
 
   getReservation(reservationId: number) {
     return this._http.get(`reservations/${reservationId}`);
+  }
+
+  cancelReservation(reservationId: number) {
+    return this._http.delete(`reservations/${reservationId}`);
+  }
+
+  payReservationCreatePayment(reservationId: number) {
+    return this._http.post(`reservations/${reservationId}`, {});
+  }
+
+  payReservationExecutePayment(paymentDTO: PaymentDTO, reservationId: number) {
+    return this._http.post(`reservations/${reservationId}/execute-payment`, paymentDTO.serialize());
   }
 }
